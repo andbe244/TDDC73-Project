3ec9094cc0e71f69dac697c030fdc8a38a04b68d
andbe244 <andreabelins@gmail.com>
2024-12-20 10:47:16 +0100

    Update DateOfBirth.tsx

--- components/DateOfBirth.tsx
+++ components/DateOfBirth.tsx
@@ -36,7 +36,7 @@ const DateOfBirth: React.FC<DateOfBirthProps> = ({ label, day, month, year, onDa
   const validateYear = (year: string) => {
     const yearInt = parseInt(year);
     const currentYear = new Date().getFullYear();
-    return isNaN(yearInt) || yearInt < 1900 || yearInt > currentYear;
+    return isNaN(yearInt) || yearInt < 1918 || yearInt > 2018;
   };
 
   const handleDayChange = (text: string) => {
